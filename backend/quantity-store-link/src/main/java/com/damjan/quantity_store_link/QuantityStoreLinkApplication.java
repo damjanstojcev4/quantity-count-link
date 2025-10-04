0d3fbd383492c3328d2b2d6c0bd1c6b6de6c0053
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-10-04 23:02:28 +0200

    Getting Started

---
+++ backend/quantity-store-link/src/main/java/com/damjan/quantity_store_link/QuantityStoreLinkApplication.java
@@ -0,0 +1,13 @@
+package com.damjan.quantity_store_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantityStoreLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantityStoreLinkApplication.class, args);
+	}
+
+}
---
+++ backend/quantity-store-link/src/test/java/com/damjan/quantity_store_link/QuantityStoreLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damjan.quantity_store_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantityStoreLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
