9f5a576db96046407984fb6ad41bb8fc17c91413
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-10-04 23:25:53 +0200

    Repository Layer

---
+++ backend/quantity-store-link/src/main/java/com/damjan/quantity_store_link/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.damjan.quantity_store_link.repository;
+
+import com.damjan.quantity_store_link.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+    Optional<Item> findItemBySku(String sku);
+}
